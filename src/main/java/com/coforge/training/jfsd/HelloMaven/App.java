a8bd6e6babd467deebaaa63085d60de8ca3ccf13
Ashish Mishra <ashishmishra1897@gmail.com>
2021-12-08 08:09:58 +0530

    Final Commit 1.0

---
+++ src/main/java/com/coforge/training/jfsd/HelloMaven/App.java
@@ -0,0 +1,17 @@
+package com.coforge.training.jfsd.HelloMaven;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello World! from Maven Project");
+	System.out.println();
+	
+	Student s1 = new Student(101,"Ashish","BTech",98);
+	
+	s1.display();
+	
+	}
+
+}
+
